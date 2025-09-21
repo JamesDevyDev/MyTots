2f12a8441ab08ca1132453b454ec4fe9bc972c5f
James Talamo <jamestalamo4@gmail.com>
2025-09-21 14:27:04 +0800

    model changes 1

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,8 +1,7 @@
 import { connectDb } from "@/utils/utils/connectDb";
 import { NextResponse } from "next/server";
 import Post from "@/utils/models/Post.Model";
-import Users from "@/utils/models/User.Model";
-
+import User from "@/utils/models/User.Model";
 //Get All post
 export const GET = async () => {
     try {
